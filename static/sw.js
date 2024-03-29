a4e1300805d9dcd8ce223bdec0048d6f185cbc3a
-k <slowdive@me.com>
2024-03-28 17:05:49 -0700

    style(pwa): reformat `onfetch`

--- static/sw.js
+++ static/sw.js
@@ -13,20 +13,21 @@ oninstall = (event) => {
 onfetch = (event) => {
   console.log("Service worker fetching", event.request.url);
   event.respondWith(caches.open(cacheName).then((cache) => {
-    return cache.match(event.request).then((cachedResponse) => {
-      const fetchedResponse = fetch(event.request).then((networkResponse) => {
-        if (networkResponse.status < 400) {
-          console.log("Caching the response to", event.request.url);
-          cache.put(event.request, networkResponse.clone());
-        } else {
-          console.log("Service worker not caching the response to", event.request.url);
-        }
+    return cache.match(event.request)
+      .then((cachedResponse) => {
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          if (networkResponse.status < 400) {
+            console.log("Caching the response to", event.request.url);
+            cache.put(event.request, networkResponse.clone());
+          } else {
+            console.log("Service worker not caching the response to", event.request.url);
+          }
 
-        return networkResponse;
-      }).catch(() => caches.match("/offline/"));
+          return networkResponse;
+        }).catch(() => caches.match("/offline/"));
 
-      return cachedResponse || fetchedResponse;
-    });
+        return cachedResponse || fetchedResponse;
+      });
   }));
 };
 
